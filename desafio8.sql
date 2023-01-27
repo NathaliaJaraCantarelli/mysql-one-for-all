eaafaf12ec573a9eb0c6b4a3fc1967346707217b
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-01-27 16:36:07 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT ar.nome_artista AS artista,
+    al.nome_album AS album
+FROM SpotifyClone.artistas AS ar
+INNER JOIN SpotifyClone.albuns AS al
+    ON ar.artista_id = al.artista_id
+WHERE ar.nome_artista = "Elis Regina"
+ORDER BY al.nome_album;
